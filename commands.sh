57fc004857ebc32fb7e930f15193d69709e75f56
Vikram Bhattacharjee <53374818+vbhattaccmu@users.noreply.github.com>
2023-05-17 20:32:29 +0800

    REFACTOR: commands.sh
    Set of commands to run WordCountDemo using Kafka Streams

---
+++ commands.sh
@@ -0,0 +1,25 @@
+# Create the input topic named streams-plaintext-input.
+
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic streams-plaintext-input
+
+# Create the output topic named streams-wordcount-output.
+
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic streams-wordcount-output
+
+# Open a Kafka console producer.
+
+bin/kafka-console-producer.sh --broker-list localhost:9092 --topic streams-plaintext-input
+
+# Open a Kafka console consumer using the default message formatter and the three properties given in the instructions.
+
+bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 \
+--topic streams-wordcount-output \
+--from-beginning \
+--formatter kafka.tools.DefaultMessageFormatter \
+--property print.key=true \
+--property print.value=true \
+--property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer \
+--property value.deserializer=org.apache.kafka.common.serialization.LongDeserializer
+
+# Use the kafka-run-class.sh command to run the WordCountDemo application
+bin/kafka-run-class.sh org.apache.kafka.streams.examples.wordcount.WordCountDemo
