108f8558a8ef1a9cfdf1aa5d7ee27d490650ff82
Vikram Bhattacharjee <53374818+vbhattaccmu@users.noreply.github.com>
2023-05-17 20:37:23 +0800

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -10,7 +10,7 @@ bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 -
 
 bin/kafka-console-producer.sh --broker-list localhost:9092 --topic streams-plaintext-input
 
-# Open a Kafka console consumer using the default message formatter and the three properties given in the instructions.
+# Open a Kafka console consumer using the default message formatter and the three defined properties given in README.
 
 bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 \
 --topic streams-wordcount-output \
